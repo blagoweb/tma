4a41d979f0f4aa12772a23b1b101e75d5044d3bb
Andrey <xxx@xxx.xxx>
2025-07-27 14:49:01 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="app">
+    <h1>Telegram Mini App на Vue3 🚀</h1>
+    <p>{{ userInfo }}</p>
+    <button @click="closeApp">Закрыть Mini App</button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const tg = window.Telegram.WebApp
+const userInfo = ref('')
+
+onMounted(() => {
+  tg.ready()  // уведомляем Telegram, что приложение готово
+  tg.expand() // разворачиваем приложение на весь экран
+
+  // пример получения данных пользователя
+  userInfo.value = JSON.stringify(tg.initDataUnsafe.user || {})
+})
+
+// Пример закрытия приложения
+const closeApp = () => tg.close()
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.app {
+  padding: env(safe-area-inset-top) 16px env(safe-area-inset-bottom);
+  text-align: center;
 }
 </style>

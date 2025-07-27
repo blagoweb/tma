b30e18f3417f8957d6b7f6e2a27ad4f243c7eed5
Andrey <xxx@xxx.xxx>
2025-07-27 15:18:13 +0300

    official

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,9 @@
-<template>
-  <div class="app">
-    <h1>Telegram Mini App на Vue3 🚀</h1>
-    <p>{{ userInfo }}</p>
-    <button @click="closeApp">Закрыть Mini App</button>
-  </div>
-</template>
+<script setup lang="ts">
+import { useBackButton } from '@/composables/useBackButton'
 
-<script setup>
-import { ref, onMounted } from 'vue'
-
-const tg = window.Telegram.WebApp
-const userInfo = ref('')
-
-onMounted(() => {
-  tg.ready()  // уведомляем Telegram, что приложение готово
-  tg.expand() // разворачиваем приложение на весь экран
-
-  // пример получения данных пользователя
-  userInfo.value = JSON.stringify(tg.initDataUnsafe.user || {})
-})
-
-// Пример закрытия приложения
-const closeApp = () => tg.close()
+useBackButton()
 </script>
 
-<style scoped>
-.app {
-  padding: env(safe-area-inset-top) 16px env(safe-area-inset-bottom);
-  text-align: center;
-}
-</style>
+<template>
+    <RouterView />
+</template>

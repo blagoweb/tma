6dfde6782f86e28518d21e48a78538ed91796ff5
Andrey <xxx@xxx.xxx>
2025-08-14 14:12:54 +0300

    add preloader

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,12 +3,13 @@
     <p>
       Создай мини-лендинг для соцсетей с аналитикой, заявками и платежами в Telegram — быстро, удобно, без кода.
     </p>
+    <pre>{{user}}</pre>
   </AppPage>
 </template>
 
 <script setup lang="ts">
 import { useAppStore } from '@/stores/app'
 const appStore = useAppStore()
-const { userName } = appStore
+const { user, userName } = appStore
 const title = `Привет ${userName}`
 </script>

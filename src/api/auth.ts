4e615845dd31b048912dce4dce7c67f0e40ba755
Andrey <xxx@xxx.xxx>
2025-08-14 14:57:10 +0300

    fix errorHandler

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,6 +2,7 @@ import api from "@/api/index";
 import {setAuthKey} from "@/helperts/storage";
 import type {TelegramUser} from "@/types/user";
 import {initData} from "@telegram-apps/sdk-vue";
+import {errorHandler} from "@/errorHandler";
 
 interface AuthResponse {
     data: {
@@ -14,9 +15,9 @@ export const authTelegram = async () => {
     try {
         const res: AuthResponse = await api.post('/auth/telegram', { init_data: initData.raw() })
         setAuthKey(res.data.token)
+        errorHandler(JSON.stringify(res))
         return res.data.user
-    } catch (e) {
-        console.error('Auth error:', e)
+    } catch {
         return null
     }
 }
--- src/errorHandler.ts
+++ src/errorHandler.ts
@@ -1,18 +1,18 @@
-import type { AppConfig } from "vue";
+// import type { AppConfig } from "vue";
 
-export const errorHandler: AppConfig['errorHandler'] = (err: unknown) => {
-    const error = err instanceof Error
-        ? err.message
-        : typeof err === 'string'
-            ? err
-            : JSON.stringify(err)
+export const errorHandler = (err: string) => {
+    // const error = err instanceof Error
+    //     ? err.message
+    //     : typeof err === 'string'
+    //         ? err
+    //         : JSON.stringify(err)
     const root = document.getElementById('app') ?? document.body
     root.insertAdjacentHTML('beforeend', `
             <div>
                 <p>An unhandled error occurred:</p>
                 <blockquote>
                     <code>
-                    ${error}
+                    ${err}
                     </code>
                 </blockquote>
             </div>

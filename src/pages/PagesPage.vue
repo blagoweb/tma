1e0b7b85262d2ff898abb3f2cd689ff29890f606
Andrey <xxx@xxx.xxx>
2025-08-15 15:20:41 +0300

    fix

--- src/pages/PagesPage.vue
+++ src/pages/PagesPage.vue
@@ -1,14 +1,14 @@
 <template>
   <AppPage title="Ваши страницы" :back="false">
-    <pre>{{pages}}</pre>
+    asdf
   </AppPage>
 </template>
 
-<script setup lang="ts">
-import {getPages} from "@/api/pages";
+<!--<script setup lang="ts">-->
+<!--import {getPages} from "@/api/pages";-->
 
-const pages = ref([])
-onBeforeMount(async () => {
-  pages.value = await getPages()
-})
-</script>
+<!--const pages = ref([])-->
+<!--onBeforeMount(async () => {-->
+<!--  pages.value = await getPages()-->
+<!--})-->
+<!--</script>-->
